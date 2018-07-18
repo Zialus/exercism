e950c602083fbc74b50c02e20e4db61a46c3726b
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-18 01:43:41 +0100

    Solved Armstrong Numbers TypeScript exercise.

---
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -0,0 +1,15 @@
+class ArmstrongNumbers {
+    static isArmstrongNumber(originalNumber: number): boolean {
+        const stringRepresentation = originalNumber.toString()
+        const size = stringRepresentation.length
+        let armstrongNumber = 0
+        for (const digit of stringRepresentation) {
+            const backToNumber = parseInt(digit, 10)
+            armstrongNumber += Math.pow(backToNumber, size)
+        }
+
+        return armstrongNumber === originalNumber
+    }
+}
+
+export default ArmstrongNumbers
