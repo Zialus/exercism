0aa8e8c5df6cc1ef3515f835b24b8859aa0df92b
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-23 16:20:24 +0100

    Improved solution for Grains C exercise.

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -2,12 +2,14 @@
 #include <math.h>
 
 unsigned long long square(int n) {
-    if (n > BOARD_SIZE) {
-        return 0;
+    if (n < 1 || n > 64) {
+        return 0; // only operate whithin the chess boards(and data type) limits.
     }
-    return powl(2, n - 1);
+    return (1ul << (n - 1)); // shifting bits to the left is the same as multiplying by 2.
 }
 
 unsigned long long total() {
-    return powl(2, BOARD_SIZE) - 1;
+    // (2**64)-1 is the largest number that can be represented by an unsigned 64-bits int,
+    // since this is an unsigned type, -1 works the same as (LARGEST_NUM_REPRESENTABLE-1).
+    return -1;
 }
--- c/grains/src/grains.h
+++ c/grains/src/grains.h
@@ -1,8 +1,6 @@
 #ifndef GRAINS_H
 #define GRAINS_H
 
-#define BOARD_SIZE 64
-
 unsigned long long square(int n);
 
 unsigned long long total();
