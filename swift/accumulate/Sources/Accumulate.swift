94def2ef92b4c102b7e08b4241ed3fe2d9bf182d
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-29 21:45:03 +0100

    Improved solution for Accumulate swift exercise.

--- swift/accumulate/Sources/Accumulate.swift
+++ swift/accumulate/Sources/Accumulate.swift
@@ -1,10 +1,9 @@
-extension Collection {
-    func accumulate<T>(_ function: (Generator.Element) -> T?) -> [T] {
+extension Sequence {
+    func accumulate<T>(_ function: (Self.Element) -> T) -> [T] {
         var result: [T] = []
         for elem in self {
-            if let res = function(elem) {
-                result.append(res)
-            }
+            let res = function(elem)
+            result.append(res)
         }
         return result
     }
