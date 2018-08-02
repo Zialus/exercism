20d8db0cc268205a1e886087da1ae5f70bf5085e
Raul Ferreira <raulmendesferreira@gmail.com>
2018-08-02 18:50:59 +0100

    Solved Two Fer C exercise.

---
+++ c/two-fer/src/two_fer.c
@@ -0,0 +1,12 @@
+#include "two_fer.h"
+#include <stdio.h>
+
+void two_fer(char* buffer, const char* name) {
+
+    if (name == NULL) {
+        name = "you";
+    }
+
+    sprintf(buffer, "One for %s, one for me.", name);
+
+}
