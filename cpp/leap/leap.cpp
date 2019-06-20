10bf618bff3921880a86e7a3fc5f628af3e7cddd
Raul Ferreira <raulmendesferreira@gmail.com>
2019-06-21 00:41:58 +0100

    Updated solution for Leap cpp exercise.

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -3,7 +3,7 @@
 namespace leap {
 
 bool is_leap_year(int year) {
-    return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) );
+    return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 }
 
-}
+}  // namespace leap
