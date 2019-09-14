1c798272e49921b980519ddf51b70101fc4c0fdc
Raul Ferreira <raulmendesferreira@gmail.com>
2019-09-14 18:50:41 +0100

    Improved solution for Roman Numerals C exercise.

--- c/roman-numerals/src/roman_numerals.c
+++ c/roman-numerals/src/roman_numerals.c
@@ -7,6 +7,58 @@
 
 #define MAX_SIZE 100
 
+char* answer(const char* start_roman_char,
+             const char* middle_roman_char,
+             const char* end_roman_char,
+             const int arab_digit) {
+    char* roman_numeral = (char*)malloc(sizeof(char) * MAX_SIZE);
+
+    roman_numeral[0] = '\0';
+
+    switch (arab_digit) {
+        case 1:
+            strcat(roman_numeral, start_roman_char);
+            break;
+        case 2:
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            break;
+        case 3:
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            break;
+        case 4:
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, middle_roman_char);
+            break;
+        case 5:
+            strcat(roman_numeral, middle_roman_char);
+            break;
+        case 6:
+            strcat(roman_numeral, middle_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            break;
+        case 7:
+            strcat(roman_numeral, middle_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            break;
+        case 8:
+            strcat(roman_numeral, middle_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, start_roman_char);
+            break;
+        case 9:
+            strcat(roman_numeral, start_roman_char);
+            strcat(roman_numeral, end_roman_char);
+            break;
+    }
+
+    return roman_numeral;
+}
+
 char* to_roman_numeral(const int number) {
     char* roman_numeral = (char*)malloc(sizeof(char) * MAX_SIZE);
 
@@ -26,103 +78,25 @@ char* to_roman_numeral(const int number) {
     // printf("C_total: %d\n", C_total);
     // printf("C_remainder: %d\n", C_remainder);
 
-    switch (C_total) {
-        case 1:
-            strcat(roman_numeral, "C");
-            break;
-        case 2:
-            strcat(roman_numeral, "CC");
-            break;
-        case 3:
-            strcat(roman_numeral, "CCC");
-            break;
-        case 4:
-            strcat(roman_numeral, "CD");
-            break;
-        case 5:
-            strcat(roman_numeral, "D");
-            break;
-        case 6:
-            strcat(roman_numeral, "DC");
-            break;
-        case 7:
-            strcat(roman_numeral, "DCC");
-            break;
-        case 8:
-            strcat(roman_numeral, "DCCC");
-            break;
-        case 9:
-            strcat(roman_numeral, "CM");
-            break;
-    }
+    char* converted_part_C = answer("C", "D", "M", C_total);
+    strcat(roman_numeral, converted_part_C);
+    free(converted_part_C);
 
     int X_total = C_remainder / 10;
     int X_remainder = C_remainder % 10;
     // printf("X_total: %d\n", X_total);
     // printf("X_remainder: %d\n", X_remainder);
 
-    switch (X_total) {
-        case 1:
-            strcat(roman_numeral, "X");
-            break;
-        case 2:
-            strcat(roman_numeral, "XX");
-            break;
-        case 3:
-            strcat(roman_numeral, "XXX");
-            break;
-        case 4:
-            strcat(roman_numeral, "XL");
-            break;
-        case 5:
-            strcat(roman_numeral, "L");
-            break;
-        case 6:
-            strcat(roman_numeral, "LX");
-            break;
-        case 7:
-            strcat(roman_numeral, "LXX");
-            break;
-        case 8:
-            strcat(roman_numeral, "LXXX");
-            break;
-        case 9:
-            strcat(roman_numeral, "XC");
-            break;
-    }
+    char* converted_part_X = answer("X", "L", "C", X_total);
+    strcat(roman_numeral, converted_part_X);
+    free(converted_part_X);
 
     int I_total = X_remainder / 1;
     // printf("I_total: %d\n", I_total);
 
-    switch (I_total) {
-        case 1:
-            strcat(roman_numeral, "I");
-            break;
-        case 2:
-            strcat(roman_numeral, "II");
-            break;
-        case 3:
-            strcat(roman_numeral, "III");
-            break;
-        case 4:
-            strcat(roman_numeral, "IV");
-            break;
-        case 5:
-            strcat(roman_numeral, "V");
-            break;
-        case 6:
-            strcat(roman_numeral, "VI");
-            break;
-        case 7:
-            strcat(roman_numeral, "VII");
-            break;
-        case 8:
-            strcat(roman_numeral, "VIII");
-            break;
-        case 9:
-            strcat(roman_numeral, "IX");
-            break;
-    }
+    char* converted_part_I = answer("I", "V", "X", I_total);
+    strcat(roman_numeral, converted_part_I);
+    free(converted_part_I);
 
     return roman_numeral;
 }
