6e5620d6525d46fcccba0789c447cc0d137f86f2
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 05:58:28 +0000

    Updated solution for Two Fer typescript exercise.

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -1,7 +1,3 @@
-class TwoFer {
-  static twoFer( name: string = "you") {
-    return `One for ${name}, one for me.`
-  }
+export function twoFer(name: string = "you"): string {
+  return `One for ${name}, one for me.`;
 }
-
-export default TwoFer
