ecb47de143ed8818a235a3e1de4329009ca6e203
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 23:29:36 +0000

    Improved solution for hamming c exercise.

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -5,7 +5,7 @@ int diff_chars(char* dna_str1, char* dna_str2, int size) {
     int count = 0;
     for (int i = 0; i < size; ++i) {
         if (dna_str1[i] != dna_str2[i]) {
-            count+=1;
+            count += 1;
         }
     }
     return count;
@@ -16,13 +16,13 @@ int compute(char* dna_str1, char* dna_str2) {
     if (dna_str1 && dna_str2) {
         int len_a = strlen(dna_str1);
         int len_b = strlen(dna_str2);
-        if (len_a != len_b) {
-            answer = -1; // dna strings size wasn't the same
-        } else {
-            answer = diff_chars(dna_str1, dna_str2, len_a); // case where we can compare them
+        if (len_a != len_b) {  // dna strings size wasn't the same
+            answer = -1;
+        } else {  // case where we can compare them
+            answer = diff_chars(dna_str1, dna_str2, len_a);
         }
-    } else {
-        answer = -1; // one of the two pointers was NULL
+    } else {  // one of the two pointers was NULL
+        answer = -1;
     }
     return answer;
 }
