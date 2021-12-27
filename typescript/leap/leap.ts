242bff89b3bce9f24ede586ff80b975626c084ae
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 06:07:21 +0000

    Updated solution for Leap typescript exercise.

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,7 +1,5 @@
-function isLeapYear( year: number ) {
-    if (year % 4 !== 0) { return false }
-    if (year % 400 === 0) { return true }
-    return (year % 100 !== 0)
+export function isLeap(year: number): boolean {
+  if (year % 4 !== 0) { return false; }
+  if (year % 400 === 0) { return true; }
+  return year % 100 !== 0;
 }
-
-export default isLeapYear
