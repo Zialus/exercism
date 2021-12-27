3fe791e19d03dec8ed67aa30dd577f100c2475f0
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 06:24:15 +0000

    Updated solution for collatz conjecture typescript exercise.

--- typescript/collatz-conjecture/collatz-conjecture.ts
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -1,18 +1,16 @@
-class CollatzConjecture {
-    static steps(n: number): number {
-        if (n <= 0) { throw Error("Only positive numbers are allowed") }
+export function steps(n: number): number {
+  if (n <= 0) {
+    throw Error("Only positive numbers are allowed");
+  }
 
-        let num_steps = 0
-        while (n !== 1) {
-            if (n % 2 === 0) {
-                n = n / 2
-            } else {
-                n = 3 * n + 1
-            }
-            num_steps += 1
-        }
-        return num_steps
+  let num_steps = 0;
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
     }
+    num_steps += 1;
+  }
+  return num_steps;
 }
-
-export default CollatzConjecture
