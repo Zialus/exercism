49502577ea738b6116885e1ee26cbe71223f0255
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 23:43:17 +0000

    Updated solution for grains c exercise.

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -8,7 +8,7 @@ unsigned long long square(int n) {
     return (1ul << (n - 1)); // shifting bits to the left is the same as multiplying by 2.
 }
 
-unsigned long long total() {
+unsigned long long total(void) {
     // (2**64)-1 is the largest number that can be represented by an unsigned 64-bits int,
     // since this is an unsigned type, -1 works the same as (LARGEST_NUM_REPRESENTABLE-1).
     return -1;
--- c/grains/grains.h
+++ c/grains/grains.h
@@ -3,6 +3,6 @@
 
 unsigned long long square(int n);
 
-unsigned long long total();
+unsigned long long total(void);
 
 #endif
