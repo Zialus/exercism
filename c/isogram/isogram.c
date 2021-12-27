49e9214154cb3ac8b14ba4536a3728b5fdebc7f4
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 23:02:57 +0000

    Improved solution for isogram c exercise.

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,14 +1,13 @@
 #include "isogram.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define ALPHABET_SIZE 26
 
-bool is_isogram(const char phrase[])
-{
-    if (phrase == NULL){
+bool is_isogram(const char phrase[]) {
+    if (phrase == NULL) {
         return false;
     }
 
@@ -17,21 +16,20 @@ bool is_isogram(const char phrase[])
     memset(alphabet, false, sizeof(bool) * ALPHABET_SIZE);
 
     int length = strlen(phrase);
-    for (int i = 0; i < length; ++i)
-    {
+    for (int i = 0; i < length; ++i) {
         char lower_case_char = tolower(phrase[i]);
-        int char_code = (int) lower_case_char - (int) 'a';
+        int char_code = (int)lower_case_char - (int)'a';
 
-        if (char_code < 0 || char_code > 25) { // char not between [a-z]
+        if (char_code < 0 || char_code > 25) {  // char not between [a-z]
             continue;
         }
 
-        if (alphabet[char_code] == true) { // char has already appeared
+        if (alphabet[char_code] == true) {  // char has already appeared
             return false;
         }
 
-        alphabet[char_code] = true; // set char as seen
-
+        alphabet[char_code] = true;  // set char as seen
     }
+
     return true;
 }
