8761bde13623bf9e659624a71e426d8aeb9559cc
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-27 07:25:21 +0000

    Improved solution for Rna Transcription typescript exercise.

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,23 +1,20 @@
 class Transcriptor {
-
-    convertLetter(dnaLetter: string): string {
-        switch (dnaLetter) {
-            case "A": return "U"
-            case "C": return "G"
-            case "G": return "C"
-            case "T": return "A"
-        }
-
-        throw Error("Invalid input DNA.")
+  convertLetter(dnaLetter: string): string {
+    switch (dnaLetter) {
+      case "A": return "U";
+      case "C": return "G";
+      case "G": return "C";
+      case "T": return "A";
     }
 
-    toRna(dnaString: string): string {
-        let rnaString = ""
-        for (const letter of dnaString) {
-            rnaString += this.convertLetter(letter)
-        }
-        return rnaString
-    }
+    throw Error("Invalid input DNA.");
+  }
+
+  toRna(dnaString: string): string {
+    const rnaString = dnaString.split('').map(letter => this.convertLetter(letter)).join('');
+    
+    return rnaString;
+  }
 }
 
-export default Transcriptor
+export default Transcriptor;
