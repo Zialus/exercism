49e0197db19d7bf77590194cd0550951f5081dc0
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-28 01:19:08 +0000

    Improved solution for roman numerals c exercise.

--- c/roman-numerals/roman_numerals.h
+++ c/roman-numerals/roman_numerals.h
@@ -1,6 +1,11 @@
 #ifndef ROMAN_NUMERALS_H
 #define ROMAN_NUMERALS_H
 
+char* answer(const char* start_roman_char,
+             const char* middle_roman_char,
+             const char* end_roman_char,
+             const int arab_digit);
+
 char* to_roman_numeral(const int number);
 
 #endif
