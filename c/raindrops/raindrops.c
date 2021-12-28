09b1500ccf6e531a62697119d07d0bde4196297e
Raul Ferreira <raulmendesferreira@gmail.com>
2021-12-28 00:56:40 +0000

    Improved solution for raindrops c exercise.

--- c/raindrops/raindrops.c
+++ c/raindrops/raindrops.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void convert(char* buffer, int drops) {
+void convert(char* const buffer, const int drops) {
     buffer[0] = '\0';
 
-    if (drops % 3 == 0) { strcat(buffer,"Pling"); }
-    if (drops % 5 == 0) { strcat(buffer,"Plang"); }
-    if (drops % 7 == 0) { strcat(buffer,"Plong"); }
+    if (drops % 3 == 0) { strcat(buffer, "Pling"); }
+    if (drops % 5 == 0) { strcat(buffer, "Plang"); }
+    if (drops % 7 == 0) { strcat(buffer, "Plong"); }
 
     if (buffer[0] == '\0') {
         sprintf(buffer, "%d", drops);
--- c/raindrops/raindrops.h
+++ c/raindrops/raindrops.h
@@ -1,6 +1,6 @@
 #ifndef RAINDROPS_H
 #define RAINDROPS_H
 
-void convert(char* buffer, int drops);
+void convert(char* const buffer, const int drops);
 
 #endif
